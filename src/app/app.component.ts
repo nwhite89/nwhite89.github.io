89a5c62b0010dd49a06d7d124c9ebb330a30e905
Nick White <nick.white@scoota.com>
2022-10-19 13:57:57 +0100

    404 page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,19 @@ import { Component, Inject, OnInit } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
-    constructor (@Inject(DOCUMENT) private document: Document) {
+    constructor (
+        @Inject(DOCUMENT) private document: Document,
+        private router: Router,
+    ) {
     }
 
     ngOnInit() {
         this.document.body.classList.remove('loading');
+
+        let path = localStorage.getItem('path');
+        if (path) {
+            localStorage.removeItem('path');
+            this.router.navigate([path]);
+        }
     }
 }
